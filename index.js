90bbeafa54c5160396f5338e9e1d0b6df74b211e
jjlee90 <jlee190@hotmail.com>
2022-04-05 18:26:55 -0400

    added background

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function tile(source, left, bottom, width, height) {
+    for (let h = 0; h < height; h++) {
+        for (let w = 0; w < width; w++) {
+
+            let background = newImage(source, left + w * 100, bottom + h * 100)
+            background.style.zIndex = -1
+        }
+    }
+}
+// function that will create an image and append it to the body based on the left and bottom px.
+function newImage(source, left, bottom) {
+    let object = document.createElement('img')
+    object.src = source
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+// function to create new item. Calls back newImage() and added listener to remove item
+function newItem(source, left, bottom) {
+
+    let item = newImage(source, left, bottom)
+
+    item.addEventListener('dblclick', function() {
+        item.remove()
+    })
+}
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight - horizon
+let heightOfGrass = horizon
+
+newImage("assets/green-character.gif", "100", "100");
+newImage("assets/pine-tree.png", "450", 200);
+newImage("assets/tree.png", 200, 300)
+newImage("assets/pillar.png", 350, 100)
+newImage("assets/crate.png", 150, 200)
+newImage("assets/well.png", 500, 425)
+
+newItem("assets/sword.png", 500, 405)
+newItem("assets/sheild.png", 165, 185)
+newItem("assets/staff.png", 600, 100)
+
+tile('assets/sky.png', 0, horizon, window.innerWidth / 100, heightOfSky / 100)
+tile('assets/grass.png', 0, 0, window.innerWidth / 100, heightOfGrass / 100)
